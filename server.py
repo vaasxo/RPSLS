ee0d8cc5bc393463d6694ab8304633e370c736fd
Radu <xovaasxo@gmail.com>
2022-01-02 21:40:38 +0200

    added game impl in server, sending response back to client

--- server.py
+++ server.py
@@ -1,10 +1,16 @@
 import socket
+import random
 from _thread import *
 
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 HOST = '127.0.0.1'
 PORT = '4444'
+
+threadNum = 0
+
+word_to_num = {"rock": 0, "spock": 1, "paper": 2, "lizard": 3, "scissors": 4}
+num_to_word = {0: "rock", 1: "spock", 2: "paper", 3: "lizard", 4: "scissors"}
 
 try:
     serverSocket.bind((HOST, PORT))
@@ -16,26 +22,55 @@ serverSocket.listen(3)
 print("socket listening on port: ", PORT)
 
 
-def client_handler(connection):
-    while True:
-        player_choice = connection.recv(1024)
-        player_choice.decode('UTF-8')
+def play_game(human_choice):
+    print("player has chosen: ", human_choice)
 
-        if not player_choice:
-            print("message received is invalid, cannot decode!")
-            connection.sendall(str.encode("error in receiving the message! Try again"))
-            break
-        else:
-            print("message received: ", player_choice)
+    player_number = word_to_num[human_choice]
+    server_number = random.randrange(0, 4)
+
+    server_choice = num_to_word[server_number]
+    print("server has chosen: ", server_choice)
+
+    diff = (server_number - player_number) % 5
+
+    if diff == 0:
+        result = "tie"
+    elif diff < 3:
+        result = "lose"
+    else:
+        result = "win"
+
+    return result, server_choice
+
+
+def client_handler(connection):
+    player_choice = connection.recv(1024)
+    player_choice.decode('UTF-8')
+
+    if not player_choice:
+        print("message received is invalid, cannot decode!")
+        connection.sendall(str.encode("error in receiving the message! Try again"))
+
+    else:
+        game_result, server_choice = play_game(player_choice)
+        connection.sendall(str.encode(game_result))
+        connection.sendall(str.encode(server_choice))
+
     connection.close()
-    return 0
 
 
 while True:
     (conn, addr) = serverSocket.accept()
     print("user with address: ", addr, " has connected")
+
     start_new_thread(client_handler, (conn,))
+    threadNum += 1
+
     conn.close()
     print("user with address: ", addr, " has disconnected")
+
+    if threadNum == 3:
+        break
+
 
 serverSocket.close()
